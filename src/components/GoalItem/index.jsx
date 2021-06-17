59195b6e749e6d56cfa39651c5d23e59e1dd0bf4
chrisiwebster <christinawebster.cw@gmail.com>
2021-06-17 14:48:49 +0100

    Update index.jsx

--- src/components/GoalItem/index.jsx
+++ src/components/GoalItem/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Button from "../Button";
+import "./styles.css";
 
 const GoalItem = ({ title, onClick }) => {
-  return (
-    <div className="goal-item">
-      <input type="checkbox" id={title} />
-      <label htmlFor={title}>{title}</label>
-      <Button title="Delete" onClick={onClick} />
-    </div>
-  );
+	return (
+		<div className="goal-item">
+			<input type="checkbox" id={title} />
+			<label htmlFor={title}>{title}</label>
+			<Button
+				icon={<i class="fas fa-trash"></i>}
+				onClick={onClick}
+				className="delete"
+			/>
+		</div>
+	);
 };
 
 export default GoalItem;

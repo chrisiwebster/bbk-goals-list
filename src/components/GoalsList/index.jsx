a34d49e2fe736b299c4b0962d025e701deaadc04
chrisiwebster <christinawebster.cw@gmail.com>
2021-06-17 14:49:40 +0100

    Update index.jsx

--- src/components/GoalsList/index.jsx
+++ src/components/GoalsList/index.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import GoalItem from "../GoalItem";
 
 const GoalsList = ({ goals, onDeleteClick }) => {
-  return (
-    <div className="goals-list">
-      {goals.map((goal) => (
-        <GoalItem
-          key={goal.id}
-          title={goal.title}
-          onClick={onDeleteClick(goal.id)}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className="goals-list">
+			{goals.map((goal) => (
+				<GoalItem
+					key={goal.id}
+					title={goal.title}
+					onClick={() => onDeleteClick(goal.id)}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default GoalsList;

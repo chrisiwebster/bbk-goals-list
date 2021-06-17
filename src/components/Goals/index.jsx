5e5206d4ee9e81e9c6aaab7ee506805af367206e
Chrisi Webster <christinawebster.cw@gmail.com>
2021-06-17 18:22:37 +0100

    make mobile friendly

--- src/components/Goals/index.jsx
+++ src/components/Goals/index.jsx
@@ -1,28 +1,26 @@
 import GoalsForm from "../GoalsForm";
 import GoalsList from "../GoalsList";
 
-import "./styles.css";
-
 const Goals = ({
-	title,
-	goalValue,
-	handleOnChange,
-	handleOnSubmit,
-	goals,
-	onDeleteClick,
+  title,
+  goalValue,
+  handleOnChange,
+  handleOnSubmit,
+  goals,
+  onDeleteClick,
 }) => {
-	return (
-		<div className="goals">
-			<h2>{`${title} goals`}</h2>
+  return (
+    <div className="goals">
+      <h2>{`${title} goals`}</h2>
 
-			<GoalsForm
-				value={goalValue}
-				onChange={handleOnChange}
-				onSubmit={handleOnSubmit}
-			/>
-			<GoalsList goals={goals} onDeleteClick={onDeleteClick} />
-		</div>
-	);
+      <GoalsForm
+        value={goalValue}
+        onChange={handleOnChange}
+        onSubmit={handleOnSubmit}
+      />
+      <GoalsList goals={goals} onDeleteClick={onDeleteClick} />
+    </div>
+  );
 };
 
 export default Goals;

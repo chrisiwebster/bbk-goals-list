5894f335ab0a1add5b51970dfcb9898c2f08ac1f
chrisiwebster <christinawebster.cw@gmail.com>
2021-06-17 14:49:07 +0100

    Update index.jsx

--- src/components/Goals/index.jsx
+++ src/components/Goals/index.jsx
@@ -1,26 +1,28 @@
 import GoalsForm from "../GoalsForm";
 import GoalsList from "../GoalsList";
 
-const Goals = ({
-  title,
-  goalValue,
-  handleOnChange,
-  handleOnSubmit,
-  goals,
-  onDeleteClick,
-}) => {
-  return (
-    <div className="goals">
-      <h2>{`${title} Goals`}</h2>
+import "./styles.css";
 
-      <GoalsForm
-        value={goalValue}
-        onChange={handleOnChange}
-        onSubmit={handleOnSubmit}
-      />
-      <GoalsList goals={goals} onClick={onDeleteClick} />
-    </div>
-  );
+const Goals = ({
+	title,
+	goalValue,
+	handleOnChange,
+	handleOnSubmit,
+	goals,
+	onDeleteClick,
+}) => {
+	return (
+		<div className="goals">
+			<h2>{`${title} goals`}</h2>
+
+			<GoalsForm
+				value={goalValue}
+				onChange={handleOnChange}
+				onSubmit={handleOnSubmit}
+			/>
+			<GoalsList goals={goals} onDeleteClick={onDeleteClick} />
+		</div>
+	);
 };
 
 export default Goals;

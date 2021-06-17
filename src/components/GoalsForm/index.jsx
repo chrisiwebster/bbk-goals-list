4216219a2ddf761dea91c1323d81490b226b62f3
chrisiwebster <christinawebster.cw@gmail.com>
2021-06-17 14:49:22 +0100

    Update index.jsx

--- src/components/GoalsForm/index.jsx
+++ src/components/GoalsForm/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Button from "../Button";
 
+import "./styles.css";
+
 const GoalsForm = ({ onSubmit, value, onChange }) => {
-  return (
-    <div className="goals-form">
-      <form onSubmit={onSubmit}>
-        <input type="text" value={value} onChange={onChange} />
-        <Button type="submit" title="Add Goal" />
-      </form>
-    </div>
-  );
+	return (
+		<div className="goals-form">
+			<form onSubmit={onSubmit}>
+				<input type="text" value={value} onChange={onChange} />
+				<Button
+					type="submit"
+					icon={<i class="fas fa-plus"></i>}
+					className="add"
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default GoalsForm;
